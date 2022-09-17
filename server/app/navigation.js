318cd1249c5be6ef9a45ca276d4e7b9ca501a1e3
Antonio Daivan <contato.daivan@gmail.com>
2022-09-17 17:43:04 -0300

    Inicial commit das paginas secundárias

---
+++ server/app/navigation.js
@@ -0,0 +1,17 @@
+const links = document.querySelectorAll('a.link')
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault()
+
+        const content = document.querySelector('.content')
+        
+        fetch(link.getAttribute('href'))
+            .then(response => response.text())
+            .then(newHTML => content.innerHTML = newHTML)
+
+        const paginaAtual = e.target.parentElement
+        console.log(e.target.parentElement)
+        links.forEach(link => link.parentElement.classList.remove('pagina-atual') )
+        paginaAtual.classList.add('pagina-atual')
+    })
+})
